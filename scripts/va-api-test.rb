a09e300b8ca91eedbaff207410e7e35b7b3b3103
Alex Bradley <alexander.bradley@gsa.gov>
2022-05-04 15:15:18 -0400

    script to test VA api

---
+++ scripts/va-api-test.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+require 'jwt'
+require 'net/http'
+
+payload = { inherited_proofing_auth: 'mocked-auth-code-for-testing', exp: 1651943405 }
+private_key = AppArtifacts.store.oidc_private_key
+token = JWT.encode(payload, private_key, 'RS256')
+
+uri = URI 'https://dev-api.va.gov/inherited_proofing/user_attributes'
+headers = { 'Authorization': "Bearer #{token}" }
+
+request = Net::HTTP.get_response(uri, headers)
+puts request.body
