166ea182cb5e9e00f6f4f4e6acaf151346d428e5
Alex Bradley <alexander.bradley@gsa.gov>
2022-05-05 11:59:13 -0400

    placed script into a class

--- scripts/va-api-test.rb
+++ scripts/va-api-test.rb
@@ -1,15 +1,25 @@
 #!/usr/bin/env ruby
 
-# Script to test connection to VA API, can be removed once we create code inside the IDV flow
 require 'jwt'
 require 'net/http'
 
-payload = { inherited_proofing_auth: 'mocked-auth-code-for-testing', exp: 1.day.from_now.to_i }
-private_key = AppArtifacts.store.oidc_private_key
-token = JWT.encode(payload, private_key, 'RS256')
+# Script to test connection to VA API, can be removed once we create code inside the IDV flow
+class VaApiTest
+  def run
+    uri = URI 'https://dev-api.va.gov/inherited_proofing/user_attributes'
+    headers = { Authorization: "Bearer #{jwt_token}" }
 
-uri = URI 'https://dev-api.va.gov/inherited_proofing/user_attributes'
-headers = { 'Authorization': "Bearer #{token}" }
+    response = Net::HTTP.get_response(uri, headers)
+    puts response.body
+  end
 
-response = Net::HTTP.get_response(uri, headers)
-puts response.body
+  private
+
+  def jwt_token
+    payload = { inherited_proofing_auth: 'mocked-auth-code-for-testing', exp: 1.day.from_now.to_i }
+    private_key = AppArtifacts.store.oidc_private_key
+    JWT.encode(payload, private_key, 'RS256')
+  end
+end
+
+VaApiTest.new.run if $PROGRAM_NAME == __FILE__
