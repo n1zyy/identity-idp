e1ecee42cea9d2a1d1142ea205b53f71f2beafbb
Alex Bradley <alexander.bradley@gsa.gov>
2022-05-12 12:01:32 -0400

    added code to decrypt response

--- scripts/va-api-test.rb
+++ scripts/va-api-test.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
 require 'jwt'
+require 'jwe'
 require 'net/http'
 
 # Script to test connection to VA API, can be removed once we create code inside the IDV flow
@@ -10,7 +11,7 @@ class VaApiTest
     headers = { Authorization: "Bearer #{jwt_token}" }
 
     response = Net::HTTP.get_response(uri, headers)
-    puts response.body
+    decrypt_payload(response)
   end
 
   private
@@ -19,6 +20,11 @@ class VaApiTest
     payload = { inherited_proofing_auth: 'mocked-auth-code-for-testing', exp: 1.day.from_now.to_i }
     private_key = AppArtifacts.store.oidc_private_key
     JWT.encode(payload, private_key, 'RS256')
+  end
+
+  def decrypt_payload(response)
+    payload = JSON.parse(response.body)['data']
+    JWE.decrypt(payload, AppArtifacts.store.oidc_private_key) if payload
   end
 end
 
