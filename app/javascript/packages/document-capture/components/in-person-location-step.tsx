5844db033edc0ca7af9202f10f3e61cfd1a51026
Shannon Alexander <shannonalexander@Shannons-MBP.fios-router.home>
2022-07-07 17:17:58 -0400

    copied and modified uswds collection item class to follow design

--- app/javascript/packages/document-capture/components/in-person-location-step.tsx
+++ app/javascript/packages/document-capture/components/in-person-location-step.tsx
@@ -1,5 +1,4 @@
 import { PageHeading, LocationCollectionItem, LocationCollection } from '@18f/identity-components';
-import { FormStepsButton } from '@18f/identity-form-steps';
 import { useI18n } from '@18f/identity-react-i18n';
 
 /**
@@ -23,7 +22,6 @@ function InPersonLocationStep() {
         <LocationCollectionItem header="BALTIMORE — Post Office &#8482;"> </LocationCollectionItem>
         <LocationCollectionItem header="COURAGE — Post Office &#8482;"> </LocationCollectionItem>
       </LocationCollection>
-      <FormStepsButton.Continue />
     </>
   );
 }
