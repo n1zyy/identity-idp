b51a0b47793a6868a8c6a2f44409f15409dffe9c
Jonathan Hooper <jonathan.hooper@gsa.gov>
2022-07-11 12:49:50 -0400

    Add some changes so that we can generate a jwt locally

--- scripts/va-api-test.rb
+++ scripts/va-api-test.rb
@@ -11,6 +11,7 @@ class VaApiTest
     headers = { Authorization: "Bearer #{jwt_token}" }
 
     response = Net::HTTP.get_response(uri, headers)
+    binding.pry
     decrypt_payload(response)
   end
 
@@ -18,14 +19,21 @@ class VaApiTest
 
   def jwt_token
     payload = { inherited_proofing_auth: 'mocked-auth-code-for-testing', exp: 1.day.from_now.to_i }
-    private_key = AppArtifacts.store.oidc_private_key
     JWT.encode(payload, private_key, 'RS256')
   end
 
   def decrypt_payload(response)
     payload = JSON.parse(response.body)['data']
-    JWE.decrypt(payload, AppArtifacts.store.oidc_private_key) if payload
+    JWE.decrypt(payload, private_key) if payload
+  end
+
+  def private_key
+    if Identity::Hostdata.in_datacenter? || !File.exist?('tmp/va_ip.key')
+      AppArtifacts.store.oidc_private_key
+    else
+      OpenSSL::PKey::RSA.new(File.read('tmp/va_ip.key'))
+    end
   end
 end
 
-VaApiTest.new.run if $PROGRAM_NAME == __FILE__
+puts(VaApiTest.new.run) if $PROGRAM_NAME == __FILE__
