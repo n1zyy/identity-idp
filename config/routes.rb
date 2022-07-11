2ed79de65241bbba6f55ea08b83e2358a3ab5ade
Shannon Alexander <shannonalexander@Shannons-MBP.fios-router.home>
2022-07-11 12:33:00 -0400

    add route and controller for locations

--- config/routes.rb
+++ config/routes.rb
@@ -328,6 +328,7 @@ Rails.application.routes.draw do
       get '/in_person' => 'in_person#index'
       get '/in_person/:step' => 'in_person#show', as: :in_person_step
       put '/in_person/:step' => 'in_person#update'
+      get '/in_person/usps_locations' => 'in_person/usps_locations#index'
 
       # deprecated routes
       get '/confirmations' => 'personal_key#show'
